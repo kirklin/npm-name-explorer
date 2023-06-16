c53a0cb3bf08de31614b6efcc89e2bcf03fb9fc1
Kirk Lin <linkirk@163.com>
2023-06-16 13:00:10 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,25 @@
+# npm-name-explorer
+
+[![CI][ci-image]][ci-url] [![npm][npm-image]][npm-url] [![downloads][downloads-image]][downloads-url] [![javascript_code style][code-style-image]][code-style-url]
+
+[ci-image]: https://github.com/kirklin/npm-name-explorer/actions/workflows/release.yml/badge.svg?branch=main
+[ci-url]: https://github.com/kirklin/npm-name-explorer/actions/workflows/release.yml
+[npm-image]: https://img.shields.io/npm/v/npm-name-explorer.svg
+[npm-url]: https://npmjs.org/package/npm-name-explorer
+[downloads-image]: https://img.shields.io/npm/dm/npm-name-explorer.svg
+[downloads-url]: https://npmjs.org/package/npm-name-explorer
+[code-style-image]: https://img.shields.io/badge/code__style-%40kirklin%2Feslint--config-brightgreen
+[code-style-url]: https://github.com/kirklin/eslint-config/
+
+
+## Usage
+
+### Install
+
+```bash
+pnpm i
+```
+
+## License
+
+[MIT](./LICENSE) License &copy; 2022-PRESENT [Kirk Lin](https://github.com/kirklin)
---
+++ src/index.ts
@@ -0,0 +1,92 @@
+import isUrl from "is-url-superb";
+import axios from "axios";
+import isScoped from "is-scoped";
+import registryUrl from "registry-url";
+import registryAuthToken from "registry-auth-token";
+import validate from "validate-npm-package-name";
+import orgRegex from "org-regex";
+
+/**
+ * Check if an npm package exists based on the given name.
+ * @param name - The name of the npm package to check.
+ * @param options - Additional options.
+ * @returns A boolean indicating whether the package exists or not.
+ * @throws Error if package name is not provided or if the `registryUrl` option is not a valid string URL.
+ */
+export default async function checkNpmNameExists(name: string, options: { registryUrl?: string } = {}): Promise<boolean> {
+  if (!(name.length > 0)) {
+    throw new Error("Package name required");
+  }
+
+  if (typeof options.registryUrl !== "undefined" && !(isUrl(options.registryUrl))) {
+    throw new Error("The `registryUrl` option must be a valid string URL");
+  }
+
+  const configuredRegistryUrl: string = registryUrl();
+  const organizationRegex = orgRegex({ exact: true });
+
+  // Ensure the URL always ends in a `/`
+  const normalizeUrl = (url: string): string => `${url.replace(/\/$/, "")}/`;
+
+  const npmOrganizationUrl = "https://www.npmjs.com/org/";
+
+  const request = async (packageName: string, requestOptions: { registryUrl?: string }): Promise<boolean> => {
+    const registryUrl: string = normalizeUrl(requestOptions.registryUrl || configuredRegistryUrl);
+
+    const isOrganization: boolean = organizationRegex.test(packageName);
+    if (isOrganization) {
+      packageName = packageName.replace(/[@/]/g, "");
+    }
+
+    const isValid = validate(packageName);
+
+    if (!isValid.validForNewPackages) {
+      const notices = [...isValid.warnings || [], ...isValid.errors || []].map((v: string) => `- ${v}`);
+      notices.unshift(`Invalid package name: ${packageName}`);
+      const error = new InvalidNameError(notices.join("\n"));
+      error.warnings = isValid.warnings;
+      error.errors = isValid.errors;
+      throw error;
+    }
+
+    let urlName = packageName;
+    const isScopedPackage = isScoped(packageName);
+    if (isScopedPackage) {
+      urlName = packageName.replace(/\//g, "%2f");
+    }
+
+    const authInfo = registryAuthToken(registryUrl, { recursive: true });
+    const headers: { [key: string]: string } = {};
+
+    if (authInfo?.type && authInfo.token) {
+      headers.authorization = `${authInfo.type} ${authInfo.token}`;
+    }
+
+    try {
+      if (isOrganization) {
+        await axios.head(npmOrganizationUrl + urlName.toLowerCase(), { timeout: 10000 });
+      } else {
+        await axios.head(registryUrl + urlName.toLowerCase(), { timeout: 10000, headers });
+      }
+
+      return true;
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      const { status } = error.response || {};
+
+      if (status === 404) {
+        return false;
+      }
+
+      throw error;
+    }
+  };
+
+  return request(name, options);
+}
+
+export class InvalidNameError extends Error {
+  warnings: any;
+  errors: any;
+}
