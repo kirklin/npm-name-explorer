02a31818d2ca8b14a68397f78f639505a097fd51
Kirk Lin <linkirk@163.com>
2023-09-20 17:18:17 +0800

    refactor: batch check package name availability

--- playground/src/pages/home/index.vue
+++ playground/src/pages/home/index.vue
@@ -1,49 +1,59 @@
 <script setup lang="ts">
 import { checkNpmName } from "~/apis/check-npm-name";
 
-const state = reactive({
-  packageName: "",
-  result: null,
+interface Result {
+  packageName: string;
+  exists: boolean;
+}
+const state = reactive<{
+  packageNames: string;
+  results: Result[];
+}>({
+  packageNames: "",
+  results: [],
 });
-
-const checkPackageExistence = async () => {
+const checkNpmNameExists = async (packageName: string) => {
   try {
-    state.result = await checkNpmName(state.packageName);
+    const exists: boolean = await checkNpmName(packageName); // Replace with your API endpoint
+    return { packageName, exists };
   } catch (error) {
-    console.error("Error:", error.message);
+    console.error(`Error checking package ${packageName}: ${error.message}`);
+    return { packageName, exists: false }; // Assume not available in case of an error
   }
+};
+const checkPackages = async () => {
+  // Split package names by line and remove any leading/trailing whitespace
+  const names = state.packageNames.split("\n").map(name => name.trim()).filter(Boolean);
+  // Reset results
+  state.results = [];
+  state.results = await Promise.all(names.map(name => checkNpmNameExists(name)));
 };
 </script>
 
 <template>
-  <div class="min-h-screen flex items-center justify-center">
-    <div class="w-full max-w-md p-6">
-      <h1 class="text-2xl font-semibold mb-4">
-        Check NPM Package Existence
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-8 rounded-lg shadow-md">
+      <h1 class="text-3xl font-semibold mb-4">
+        NPM Package Checker
       </h1>
-      <input
-        v-model="state.packageName"
-        class="w-full border p-2 rounded"
-        placeholder="Enter package name"
-      >
-      <button
-        class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        @click="checkPackageExistence"
-      >
-        Check Existence
-      </button>
-      <div v-if="state.result !== null" class="mt-4">
-        <p v-if="state.result" class="text-green-600 font-semibold">
-          Package exists!
-        </p>
-        <p v-else class="text-red-600 font-semibold">
-          Package does not exist.
-        </p>
+      <div class="mb-4">
+        <textarea v-model="state.packageNames" class="w-full p-2 border rounded" placeholder="Enter package names (one per line)" />
+      </div>
+      <div class="mb-4">
+        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700" @click="checkPackages">
+          Check Packages
+        </button>
+      </div>
+      <div v-if="state.results.length > 0">
+        <h2 class="text-xl font-semibold mb-2">
+          Results:
+        </h2>
+        <ul>
+          <li v-for="result in state.results" :key="result.packageName" class="mb-2">
+            <span class="font-semibold">{{ result.packageName }}</span> is <span v-if="result.exists" class="text-red-500">not available</span><span v-else class="text-green-500">available</span>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
-
-<style>
-/* Add your Tailwind CSS styles here */
-</style>
